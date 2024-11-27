2e96f8bd0a88ef01a211e8f50c1de1dbc17bc54a
limminhoooo <dlaalsgh12@gmail.com>
2024-11-27 18:53:28 +0900

    비디오 컴포넌트 통합작업

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -10,7 +10,6 @@ const Developer = () => {
             title="추천 개발자"
             description="오늘의 추천 개발자 유튜버입니다."
         >
-            Developer
             <section id="developerPage">
                 <h2>😪 추천 개발자를 소개합니다.</h2>
                 <div className="developer__inner">
