68af02117111530431229103cf7cd04ecc2b5968
limminhoooo <dlaalsgh12@gmail.com>
2024-11-28 16:52:01 +0900

    검색 및 유튜브 API 설정하기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import Main from '../components/section/Main'
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import Main from "../components/section/Main";
+
+import VideoSearch from "../components/video/VideoSearch";
 
 const Search = () => {
-    return (
-        <Main 
-            title = "유투브 검색"
-            description="유튜브 검색 결과 페이지입니다.">
-            Search
-        </Main>
-    )
-}
+    const {searchID} = useParams();
+    const [videos, setVideos] = useState([]);
 
-export default Search
+    useEffect(() => {
+        fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchID}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+        )
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+                setVideos(result.items);
+            })
+            .catch((error) => console.log(error));
+    }, [searchID]);
+
+    return (
+        <Main title="유투브 검색" description="유튜브 검색 결과 페이지입니다.">
+            <section id="searchPage">
+                <div className="video__inner search">
+                    <VideoSearch videos={videos} />
+                </div>
+            </section>
+        </Main>
+    );
+};
+
+export default Search;
